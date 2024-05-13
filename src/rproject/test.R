3d03193b8ab9a55ca3660fd2a6d60ff49f1d0f34
mintcd <dangminh2208@gmail.com>
2024-05-14 01:47:00 +0700

    End 14/5

--- src/rproject/test.R
+++ src/rproject/test.R
@@ -1,16 +1,1 @@
-# Count of NA values
-na_counts <- colSums(is.na(cpu_data))
-
-# Count of empty strings
-empty_counts <- colSums(cpu_data == "")
-
-
-
-# Create a data frame for properties with NA values
-invalid_df <- 
-  data.frame(Property = names(na_counts), 
-             NA_Count = unname(na_counts),
-             Empty_Count = unname(empty_counts[]))
-
-kable(invalid_df, format = "html") %>%
-  kable_styling()
+#bartlett.test(processed_cpu_data$nb_of_Cores ~ #processed_cpu_data$Product_Collection, data = processed_cpu_data)
